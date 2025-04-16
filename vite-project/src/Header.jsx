130b88635203bbcf758e486e32294adca2913271
mdantor383800 <mdantor383800@gmail.com>
2025-04-16 22:35:57 +0600

    add messsage

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
 function Header(){
-const[name, setName]=useState('Antor');
+const[name, setName]=useState('');
 const handleName =()=>{
-    setName('Arif');
+    setName('Antor');
 }
     return(
         <div>
             <h1>Hello</h1>
             <button onClick={handleName}>Click Me</button>
+            <button onClick={()=>setName('')}>Clear</button>
             <h2>{name}</h2>
         </div>
     )
---
+++ vite-project/src/User.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function User() {
+    const[name, setName]= useState('');
+    const[password, setPassword]= useState('');
+    const[email, setEmail]= useState('');
+
+    return(
+        <div>
+            <input type="text" value={name} onChange={(event)=>{setName(event.target.value)}} placeholder="Enter Your Name"/>
+            <br/><br/>
+            <input type="password" value={password}  onChange={(event)=>{setPassword(event.target.value)}} placeholder="Enter Password"/>
+            <br/><br/>
+            <input type="email" value={email}  onChange={(event)=>{setEmail(event.target.value)}} placeholder="Enter Your Email"/>
+            <br/><br/>
+            <button>Submit</button>
+            <button onClick={()=>{setName(''); setPassword(''); setEmail('')}}>Delete</button>
+            <br/><br/>
+            <h2>{name}</h2>
+            <h2>{password}</h2>
+            <h2>{email}</h2>
+
+        </div>
+    )
+}
+export default User;
